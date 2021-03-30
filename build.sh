d6223e49606a3f2dcb2506554b4ddd77d4e49f52
Eric Urban <hydrogen18@gmail.com>
2021-03-30 10:21:07 -0500

    Add stoppability

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -f ./goosebin
-exec go build ./cmd/goosebin/
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 exec go build ./cmd/goosebin/
--- cmd/goosebin/main.go
+++ cmd/goosebin/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
 	"time"
 )
 import "github.com/ovrclk/goosebin/internal/server"
@@ -10,16 +17,55 @@ import "github.com/ovrclk/goosebin/internal/server"
 func main() {
 	router, err := server.GetRouter()
 	if err != nil {
-		panic(err)
+		fmt.Printf("Could not create server:%v\n", err)
+		os.Exit(1)
 	}
 
+	httpPortStr := os.Getenv("HTTP_PORT")
+	if len(httpPortStr) == 0 {
+		httpPortStr = "8000"
+	}
+	httpPort, err := strconv.Atoi(httpPortStr)
+	if err != nil {
+		fmt.Printf("Could not parse http port string %q:%v\n", httpPortStr, err)
+		os.Exit(1)
+	}
+	addr := fmt.Sprintf("0.0.0.0:%d", httpPort)
 	srv := &http.Server{
 		Handler: router,
-		Addr: "0.0.0.0:8000",
+		Addr: addr,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	srv.ListenAndServe()
+	fmt.Printf("Starting HTTP server on %q\n", addr)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
+		result := <-sig
+		fmt.Printf("Got %s, stopping server\n", result)
+
+		ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(time.Second * 30))
+
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(ctx); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+
+	}()
+
+	err = srv.ListenAndServe()
+	wg.Wait()
+
+	if err != nil && err != http.ErrServerClosed {
+		fmt.Printf("HTTP server failed: %v\n", err)
+		os.Exit(1)
+	}
+
 	os.Exit(0)
 }
